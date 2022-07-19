58d46e65caeb547fcd641cc70279f499380913f8
PedroProcter <OProcter89@gmail.com>
2022-07-19 08:36:04 -0400

    feat: Implement GameBoard Class

---
+++ pyconnect4/gameboard.py
@@ -0,0 +1,27 @@
+
+
+
+
+class GameBoard:
+	"""GameBoard Docstring"""
+
+	def __init__(self, board_vertical_size: int, board_horizontal_size: int) -> None:
+		self.gameboard = []
+		
+		for rows in range(board_vertical_size):
+			row = []
+			for columns in range(board_horizontal_size):
+				row.append(0)
+
+			self.gameboard.append(row)
+
+	def place_token(self, column: int, player_token_id: int) -> list:
+		"""Place a token in the lowest position of a column"""
+		token_final_coordinates = [0, column]
+		for index, row in enumerate(self.gameboard[::-1]):
+			if row[column] == 0:
+				row[column] = player_token_id
+				token_final_coordinates[0] = index
+				break
+		
+		return token_final_coordinates
