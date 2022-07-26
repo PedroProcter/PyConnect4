3ed6c0b5a76a52a169fe2edc109469eb928a1a1a
JorgeMedranoNovas <jorgemodrano502@gmail.com>
2022-07-26 16:41:01 -0400

    Clase player

---
+++ pyconnect4/player.py
@@ -0,0 +1,12 @@
+class player:
+    def __init__(self, ID: int, Name):
+        self.ID: int = ID
+        self.Name = Name
+    
+    
+    def  get_next_move(self,Da: int):
+        
+        for self.ID in False:
+            
+
+
