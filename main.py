f087994a4d5412e87c5197ef295f0d0789398d3d
PedroProcter <OProcter89@gmail.com>
2022-07-27 09:15:23 -0400

    feat: boilerplate code for the near future

---
+++ main.py
@@ -0,0 +1,7 @@
+from pyconnect4 import game
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ pyconnect4/game.py
@@ -0,0 +1,6 @@
+from .entities import User
+from .gameboard import GameBoard
+from .game_referee import GameReferee
+
+class Game:
+    """Docstring for Game"""
