f619750a29dada66118f9c2409e0dfb748b17bb1
PedroProcter <OProcter89@gmail.com>
2022-07-27 11:09:46 -0400

    feat: Implement method test_left_diagonal_goal

--- pyconnect4/game_referee.py
+++ pyconnect4/game_referee.py
@@ -74,7 +74,36 @@ class GameReferee:
     @staticmethod
     def test_left_diagonal_goal(gameboard: GameBoard, player_token_id: int, last_token_coordinate_x: int, last_token_coordinate_y: int) -> int | bool:
         """Tests if the player have connected 4 token in (left) diagonal"""
-        raise NotImplementedError
+        tokens_collected = [str(player_token_id)]
+
+        actual_token_index_x = last_token_coordinate_x
+        actual_token_index_y = last_token_coordinate_y
+        
+        while actual_token_index_x > 0 and actual_token_index_y > 0:
+            actual_token_index_y -= 1
+            actual_token_index_x -= 1
+
+            tokens_collected.append(str(gameboard.gameboard[actual_token_index_y][actual_token_index_x]))
+
+        actual_token_index_x = last_token_coordinate_x
+        actual_token_index_y = last_token_coordinate_y
+
+        while actual_token_index_x < gameboard.columns - 1 and actual_token_index_y < gameboard.rows - 1:
+            actual_token_index_y += 1
+            actual_token_index_x += 1
+
+            tokens_collected.insert(0, str(gameboard.gameboard[actual_token_index_y][actual_token_index_x]))
+
+        if tokens_collected.count(str(player_token_id)):
+
+            tokens_collected = "".join(tokens_collected)
+
+            goal_state = f"{player_token_id}{player_token_id}{player_token_id}{player_token_id}"
+
+            if goal_state in tokens_collected:
+                return player_token_id
+
+        return False     
 
     @staticmethod
     def test_goal(gameboard: GameBoard, last_token_coordinates: tuple, player_token_id: int) -> int | bool:
