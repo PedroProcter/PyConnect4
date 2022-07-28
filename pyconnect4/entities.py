f45b1d38b03fccb44e7c2fb9f65dd2aaa9275b17
PedroProcter <OProcter89@gmail.com>
2022-07-28 09:03:17 -0400

    feat: Create Class Machine

--- pyconnect4/entities.py
+++ pyconnect4/entities.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+from .gameboard import GameBoard
+
 
 @dataclass(init=True, repr=True, frozen=True)
 class Player(ABC):
@@ -14,4 +16,12 @@ class Player(ABC):
 class User(Player):
 
     def get_next_move(self) -> int:
+        raise NotImplementedError
+
+class Machine(Player):
+
+    def get_next_move(self, gameboard: GameBoard) -> int:
+        raise NotImplementedError
+
+    def compute_minimax(self) -> int:
         raise NotImplementedError
