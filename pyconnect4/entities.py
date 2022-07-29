234c748d1cdd4489f8a6811c0a29ce22a94b21cb
JorgeMedranoNovas <jorgemodrano502@gmail.com>
2022-07-28 23:41:01 -0400

    Mejora minimax

--- pyconnect4/entities.py
+++ pyconnect4/entities.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from asyncio.windows_events import NULL
 from dataclasses import dataclass
 from .game_referee import GameReferee
 
@@ -24,41 +25,43 @@ class User(Player):
 class Machine(Player):
 
     def get_next_move(self, gameboard: GameBoard) -> int:
+        if User.get_next_move()==played_machine:           
+            movement = Machine.compute_minimax(gameboard,self.ID)
+            gameboard[played_machine]=self.ID
+            return gameboard
+
         raise NotImplementedError
 
-    def compute_minimax(gameboard: GameBoard,last_token_coordinates: tuple, player_token_id: int): #quite el int 
+    def compute_minimax(gameboard: GameBoard, player_token_id: int): #quite el int 
         global played_machine
         ##refisar
-        if GameReferee.test_goal(gameboard,last_token_coordinates,player_token_id):
-            return(gameboard.gameboard,0)
+        if GameReferee.test_goal(gameboard,player_token_id):
+            return(gameboard,0)
         #POSIBLES JUGADAS 
         movement = []
-        last_token_coordinates=gameboard.gameboard
+ 
 
-
-        while last_token_coordinates.columns[x] !=6 and last_token_coordinates.row[y] != 7:
+        while gameboard.columns[x] !=6 and gameboard.rows[y] != 7:
             x+=1
-            last_token_coordinates   =  last_token_coordinates.columns[x],last_token_coordinates.row[y]
+            last_token_coordinates   =  gameboard.columns[x],gameboard.rows[y]
 
-            if  last_token_coordinates.columns[x] !=6:
+            if  gameboard.columns[x] !=6:
                 y+=1
-                last_token_coordinates.row[y]
+                gameboard.rows[y]
 
         for move in range (0,len(last_token_coordinates)):
-            if gameboard[move]==0:
+            if gameboard[move]==NULL:
                 gameboardaux = gameboard
                 gameboardaux.place_token[move] = player_token_id
-                punctuation= Machine.compute_minimax(gameboardaux,move,player_token_id)#el menos unos estas porque no se cual es el aide del pc
+                punctuation= Machine.compute_minimax(gameboardaux,player_token_id)#el menos unos estas porque no se cual es el aide del pc
                 movement.append([punctuation,move]) 
         
-        if player_token_id == MAX:
-            movement = max(movement)
-            played_machine= movement[1] 
-            return movement
-        else:
-            movement=min(movement)
-            return movement[0]
-            s
+        #quite la condicional porque no tenia logica alguna
+        movement = max(movement)
+        played_machine= movement[1] 
+        return movement
+        
+            
 
 
 
